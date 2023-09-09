b6ad3576aa46266c99cc45acb877c11e93761200
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2023-09-09 13:27:14 -0600

    Condicionales - video#3

---
+++ Basico/Condicionales.php
@@ -0,0 +1,17 @@
+<?php
+//condicionales
+
+$numero_uno = 30;
+$numero_dos = 30;
+
+if ($numero_uno < $numero_dos) 
+ {
+    echo 'Curso de php';
+ }
+elseif ($numero_uno == $numero_dos) {
+    echo "El numero $numero_uno es igual al Numero dos";
+}
+else {
+    echo'Curso Pyton';
+}
+?>
