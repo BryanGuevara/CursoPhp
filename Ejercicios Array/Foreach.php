a887a54cd2170fcf97cc35959e5483dcad71772a
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2023-09-11 22:48:14 -0600

    Foreach - video#10

---
+++ Ejercicios Array/Foreach.php
@@ -0,0 +1,8 @@
+<?php
+$juegos = Array('Nintendo'=>'Mario', 'Ubisoft'=>'Rayman', 'PS'=>'Crash', 'Sega'=> 'Sonic');
+
+foreach ($juegos as $dato => $valor) 
+{
+    echo "$valor es la mascota de $dato<br>";
+}
+?>
