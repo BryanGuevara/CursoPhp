58e75041320ce48f71882c80e68af98d532005d7
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2023-10-04 14:52:12 -0600

    Corrigiendo detalles

--- Calculadora/Calculadora.php
+++ Calculadora/Calculadora.php
@@ -18,10 +18,10 @@
 </html>
 
 <?php
-if(isset($_GET['numero1']) && isset($_GET['numero2']) && is_numeric($_GET['numero1']) && is_numeric($_GET['numero1']));
+if(isset($_GET['numero1']) && isset($_GET['numero2']) && is_numeric($_GET['numero1']) && is_numeric($_GET['numero2']))
 {
     $numero1 = $_GET['numero1'];
     $numero2 = $_GET['numero2'];
-    echo "$numero1 + $numero2 = ".$numero1 + $numero2;
+    echo "$numero1 + $numero2 = " . ($numero1 + $numero2);
 }
 ?>
